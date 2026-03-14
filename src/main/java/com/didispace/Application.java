39136ebc26422effaf09c5c8aa7db097ed0c520f
cmos-array <702386730@qq.com>
2026-03-14 15:22:09 +0800

    update for test

--- src/main/java/com/didispace/Application.java
+++ src/main/java/com/didispace/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author CMOS_ARRAY
  * @version 1.0.0
  * @date 10/9/18 下午7:04.
- * @sina CMOS_array
+ * @sina CMOS_array 
  */
 @SpringBootApplication
 public class Application {
